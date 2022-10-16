82ec0aeeaf180c239c97046d1523b1f0f9cb54a1
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-16 10:16:42 +0530

    Update polls.module.ts
    register polls schema to polls module

--- src/polls/polls.module.ts
+++ src/polls/polls.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { Poll, PollSchema } from './schemas/poll.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Poll.name, schema: PollSchema }]),
+  ],
   controllers: [PollsController],
-  providers: [PollsService]
+  providers: [PollsService],
 })
 export class PollsModule {}
