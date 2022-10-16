6fe7e7de8eb93d6e904415383d5e143e1044a37a
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-16 09:05:43 +0530

    configure polls
    - create starter files for:
      - polls module
      - polls controller
      - polls service
    
    - move polls schema to its folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), PollsModule],
   controllers: [AppController],
   providers: [AppService],
 })
