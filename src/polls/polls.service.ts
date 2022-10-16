2e8629ff968ddb415fd3040f19a44661f4ec2856
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-16 10:17:07 +0530

    Update polls.service.ts
    inject polls schema into polls service

--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -1,4 +1,19 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Poll, PollDocument } from './schemas/poll.schema';
+import { CreatePollDto } from './dto/create-poll.dto';
 
 @Injectable()
-export class PollsService {}
+export class PollsService {
+  constructor(@InjectModel(Poll.name) private pollModel: Model<PollDocument>) {}
+
+  async create(createPollDto: CreatePollDto): Promise<Poll> {
+    const createdPoll = new this.pollModel(createPollDto);
+    return createdPoll.save();
+  }
+
+  async findAll(): Promise<Poll[]> {
+    return this.pollModel.find().exec();
+  }
+}
