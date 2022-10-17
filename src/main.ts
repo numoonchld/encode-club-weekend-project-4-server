e9c6275def5524c5f84fa5ac6e750b7dfe36b791
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-17 06:55:20 +0530

    Update main.ts
    enable built-in cors support

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // enable CORS for API calls from Angular
+  app.enableCors();
+
   // configure OpenAPI (Swagger)
   const config = new DocumentBuilder()
     .setTitle('Tokenized Ballot dApp')
